8b128f06c02d4b003aa2fb859f5efbe3d58017ab
vagnero <vagnerimperador16@gmail.com>
2024-03-07 19:42:58 -0300

    Criação da classe de configurações do cors e conexão com o frontend

---
+++ src/config/WebConfig.java
@@ -0,0 +1,15 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir solicitações de todas as origens
+                .allowedOrigins("http://localhost:3000") // Permitir solicitações apenas de http://localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitir métodos específicos
+                .allowedHeaders("*"); // Permitir todos os headers
+    }
+}
