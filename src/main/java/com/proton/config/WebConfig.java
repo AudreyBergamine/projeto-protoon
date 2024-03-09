5d88301f391a1db5e6a2e636bf4b1c772bb01fd3
vagnero <vagnerimperador16@gmail.com>
2024-03-09 12:01:34 -0300

    Foi necessário criar uma classe para que o spring boot aceitasse a data criada no front-end (deserializer).

--- src/main/java/com/proton/config/WebConfig.java
+++ src/main/java/com/proton/config/WebConfig.java
@@ -1,3 +1,5 @@
+package com.proton.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
