87befbe42ead5bd4e3c820d56b3e97e8ffb45a4e
Wesley Paulo Siqueira Soares <wesleyoares7@gmail.com>
2024-03-22 10:11:09 -0300

    up

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
--- Password: password, há uma variável no codigo qeu armazena a senha em password
--- Aqui você deve substituir a senha criptografada correta
-INSERT INTO USERS (username, password, role) VALUES ('admin', 'admin', 'ADMIN');
