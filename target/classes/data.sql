eb5b9779278359f1bcbc9e1c17597cc2f55f8c28
vagnero <vagnerimperador16@gmail.com>
2024-03-30 08:07:57 -0300

    cc

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+-- Password: password, há uma variável no codigo qeu armazena a senha em password
+-- Aqui você deve substituir a senha criptografada correta
+INSERT INTO USERS (username, password, role) VALUES ('admin', '$2a$10$ESNLdQaJCXulLDzfd3/OgeE4thz3BDceJJBQTFQgGEnsLgeA3ZIXG', 'ADMIN');
