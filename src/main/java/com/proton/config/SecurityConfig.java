c1f95c647ed6e72ceae86225b034f71d24fbad15
Wesley Paulo Siqueira Soares <wesleyoares7@gmail.com>
2024-03-30 14:50:34 -0300

    up

--- src/main/java/com/proton/config/SecurityConfig.java
+++ src/main/java/com/proton/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +27,7 @@ import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
   @Value("${jwt.public.key}")
   private RSAPublicKey key;
