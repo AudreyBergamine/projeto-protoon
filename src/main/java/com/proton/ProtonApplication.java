a596d30f36a00edd5bdde22385c4b692aa0d0077
Wesley Paulo Siqueira Soares <wesleyoares7@gmail.com>
2024-04-11 07:14:09 -0300

    up

--- src/main/java/com/proton/ProtonApplication.java
+++ src/main/java/com/proton/ProtonApplication.java
@@ -4,6 +4,9 @@ package com.proton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class ProtonApplication {
@@ -12,4 +15,9 @@ public class ProtonApplication {
 		SpringApplication.run(ProtonApplication.class, args);
 		System.out.println("\nRodando...\n");
 	}
+
+	@Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
 }
